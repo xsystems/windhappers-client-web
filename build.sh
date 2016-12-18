a54e8e218cf6e841fe7d3ad432e2a396012855b5
Koen Boes <koenboes@gmail.com>
2016-12-18 13:48:31 +0100

    Fix typo in `build.sh`

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ rm -rf bower_components build
 bower install
 polymer build --sw-precache-config sw-precache-config.js
 cp docker/* build/
-docker build -t xsystems/windhappes-client-web build
+docker build -t xsystems/windhappers-client-web build
