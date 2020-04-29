3d738d7fe88acba5a0bb0fadf49689ec3e9a7fb4
Koen Boes <kboes@qualogy.com>
2020-04-29 23:37:30 +0200

    Remove current web application

--- README.md
+++
@@ -1,1 +0,0 @@
-# Windhappers Client Web
--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-rm -rf bower_components build
-bower install
-polymer build --sw-precache-config sw-precache-config.js
-cp docker/* build/
-docker build -t xsystems/windhappers-client-web build
