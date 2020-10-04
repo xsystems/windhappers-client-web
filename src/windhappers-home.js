0aaf1943b37b2e795d4a37bb7f36f3af2d3f2a1d
Koen Boes <kboes@qualogy.com>
2020-10-04 21:15:27 +0200

    Update COVID-19 statement on home page

--- src/windhappers-home.js
+++ src/windhappers-home.js
@@ -1,9 +1,9 @@
 import { LitElement, html, css } from 'lit-element';
-import { windhappersStyles } from './windhappers-styles';
+import { windhappersStyles } from './windhappers-styles.js';
 
-import './windhappers-notification'
-import './components/xsystems-google-calendar'
-import './components/xsystems-youtube-video'
+import './windhappers-notification.js';
+import './components/xsystems-google-calendar.js';
+import './components/xsystems-youtube-video.js';
 
 export class WindhappersHome extends LitElement {
   static get styles() {
@@ -39,7 +39,7 @@ export class WindhappersHome extends LitElement {
           box-shadow: var(--shadow-elevation-4dp);
         }
 
-        img, 
+        img,
         xsystems-youtube-video {
           width: 100%;
           box-shadow: var(--shadow-elevation-4dp);
@@ -68,77 +68,105 @@ export class WindhappersHome extends LitElement {
           background-color: var(--primary-color);
         }
 
-        th, td {
+        th,
+        td {
           text-align: left;
           padding: 1%;
         }
 
         tr:nth-child(even) {
-          background-color: #f2f2f2
+          background-color: #f2f2f2;
         }
-      `
-    ]
+      `,
+    ];
   }
 
   static get properties() {
     return {
       narrow: {
         type: Boolean,
-        reflect: true
+        reflect: true,
       },
       _calendars: {
-        type: Array
-      }
+        type: Array,
+      },
     };
   }
 
   constructor() {
     super();
-    this._calendars = ['windhappers.nl_djorriihnjatt2p3it67t8v2bo@group.calendar.google.com', 'nl.dutch#holiday@group.v.calendar.google.com'];
+    this._calendars = [
+      'windhappers.nl_djorriihnjatt2p3it67t8v2bo@group.calendar.google.com',
+      'nl.dutch#holiday@group.v.calendar.google.com',
+    ];
   }
 
   render() {
     return html`
       <windhappers-notification id="covid19" type="warning">
-        In verband met de versoepeling van de <b>COVID-19</b> richtlijnen is onze kantine per <b>1 juli</b> weer geopend. 
-        Tevens kan er weer gebruik worden gemaakt van de kleedkamers, douches en toiletten.
-        Neem te allen tijde <a target="_blank" rel="noopener" title="De richtlijnen van het RIVM" href="https://www.rijksoverheid.nl/onderwerpen/coronavirus-covid-19/veelgestelde-vragen-per-onderwerp/sport">de richtlijnen van het RIVM</a> in acht!
-        
-        Ons <b><a href="https://storage.googleapis.com/windhappers-site/association_documents/coronaprotocol_2020-05-12_v4.pdf" title="Coronaprotocol" target="_blank" rel="noopener">coronaprotocol is hier</a></b> te vinden of kijk onder <a href="/documents" title="Documenten">Documenten</a>.
+        In verband met de aanscherping van de <b>COVID-19</b> richtlijnen is onze kantine per
+        <b>29 september 2020</b> tot nader order gesloten. De kleedkamers, douches en toiletten
+        blijven wel open voor gebruik. Neem te allen tijde
+        <a
+          target="_blank"
+          rel="noopener"
+          title="De richtlijnen van het RIVM"
+          href="https://www.rijksoverheid.nl/onderwerpen/coronavirus-covid-19/veelgestelde-vragen-per-onderwerp/sport"
+          >de richtlijnen van het RIVM</a
+        >
+        in acht! Ons
+        <b
+          ><a
+            href="https://storage.googleapis.com/windhappers-site/association_documents/coronaprotocol_2020-05-12_v4.pdf"
+            title="Coronaprotocol"
+            target="_blank"
+            rel="noopener"
+            >coronaprotocol is hier</a
+          ></b
+        >
+        te vinden of kijk onder <a href="/documents" title="Documenten">Documenten</a>.
       </windhappers-notification>
 
       <article id="club-magazine">
-        <b>Op zoek naar ons clubblad "De Windvlaag"? Kijk onder <a href="/documents" title="Documenten">Documenten</a>.</b>
+        <b
+          >Op zoek naar ons clubblad "De Windvlaag"? Kijk onder
+          <a href="/documents" title="Documenten">Documenten</a>.</b
+        >
       </article>
 
       <article id="who">
         <header>
           <h1>Welkom!</h1>
           <p>
-            Kanovereniging De Windhappers is dé Haagse kanovereniging, gelegen in het recreatiegebied De Uithof in Den Haag. 
+            Kanovereniging De Windhappers is dé Haagse kanovereniging, gelegen in het
+            recreatiegebied De Uithof in Den Haag.
           </p>
         </header>
         <section>
           <p>
-            Wij zijn een actieve kanovereniging met ongeveer 200 leden, van jong tot oud. We beschikken over een
-            prachtig verenigingsgebouw voorzien van een gezellige kantine, een scala aan faciliteiten, een eigen haven,
-            en botenloodsen. Onze leden doen aan (bijna) alle <a href="/disciplines" title="Disciplines">vormen van kanosport</a>: 
-            toervaren (zowel in kajak als Canadese kano); kanopolo; zeevaren; brandingvaren; en wildwatervaren.           
+            Wij zijn een actieve kanovereniging met ongeveer 200 leden, van jong tot oud. We
+            beschikken over een prachtig verenigingsgebouw voorzien van een gezellige kantine, een
+            scala aan faciliteiten, een eigen haven, en botenloodsen. Onze leden doen aan (bijna)
+            alle <a href="/disciplines" title="Disciplines">vormen van kanosport</a>: toervaren
+            (zowel in kajak als Canadese kano); kanopolo; zeevaren; brandingvaren; en
+            wildwatervaren.
           </p>
 
           <p>
-            Elke woensdagavond is onze clubavond, kom gerust eens langs voor <a href="/membership">informatie</a> of maak een afspraak
-            voor een proefvaart met een clubboot, geheel gratis en vrijblijvend.
+            Elke woensdagavond is onze clubavond, kom gerust eens langs voor
+            <a href="/membership">informatie</a> of maak een afspraak voor een proefvaart met een
+            clubboot, geheel gratis en vrijblijvend.
           </p>
 
           <p>
-            We verzorgen cursussen in kajakvaren, kanovaren, kanopolo, en zeevaren. Ook geven we lessen in
-            eskimoteren in een verwarmd zwembad. 
+            We verzorgen cursussen in kajakvaren, kanovaren, kanopolo, en zeevaren. Ook geven we
+            lessen in eskimoteren in een verwarmd zwembad.
           </p>
 
           <p>
-            We organiseren regelmatig, uitdagende en gezellige <a href="/calendar" title="Kalendar">activiteiten</a>, 
-            toertochten op vlakwater en op zee; kano kampeerweekenden; kanopolo competitie; en wildwatervaren in het buitenland. 
+            We organiseren regelmatig, uitdagende en gezellige
+            <a href="/calendar" title="Kalendar">activiteiten</a>, toertochten op vlakwater en op
+            zee; kano kampeerweekenden; kanopolo competitie; en wildwatervaren in het buitenland.
           </p>
         </section>
       </article>
@@ -147,30 +175,43 @@ export class WindhappersHome extends LitElement {
         <header>
           <h1>NK Kanopolo</h1>
           <p>
-            Het Weekend van 7 en 8 september 2019 werd er bij Kanovereniging De Windhappers weer het Nederlands Kampioenschap kanopolo gespeeld.
-            Nadat eerder in het seizoen al was gespeeld streden tientalle teams, op 4 velden voor het clubgebouw 
-            en op de Wen, om de fel begeerde titel van Nederlands kampioen. Het was weer een geslaagd evenement!
+            Het Weekend van 7 en 8 september 2019 werd er bij Kanovereniging De Windhappers weer het
+            Nederlands Kampioenschap kanopolo gespeeld. Nadat eerder in het seizoen al was gespeeld
+            streden tientalle teams, op 4 velden voor het clubgebouw en op de Wen, om de fel
+            begeerde titel van Nederlands kampioen. Het was weer een geslaagd evenement!
           </p>
         </header>
 
         <xsystems-youtube-video video-id="JIRRUBh4hrM"></xsystems-youtube-video>
 
         <p>
-          <a href="https://www.omroepwest.nl/nieuws/amp/3897931/Spetters-vliegen-in-het-rond-tijdens-finaleweekend-NK-Kanopolo-Den-Haag" target="_blank" rel="noopener">Zie hier het volledige Omroep West artikel.</a>
+          <a
+            href="https://www.omroepwest.nl/nieuws/amp/3897931/Spetters-vliegen-in-het-rond-tijdens-finaleweekend-NK-Kanopolo-Den-Haag"
+            target="_blank"
+            rel="noopener"
+            >Zie hier het volledige Omroep West artikel.</a
+          >
         </p>
 
         <xsystems-youtube-video video-id="g_JLCOqdJ8A"></xsystems-youtube-video>
 
         <p>
-          <a href="https://www.wos.nl/nk-kanopolo-op-de-wen-heel-fysiek-je-mag-heel-veel/nieuws/item?1142346" target="_blank" rel="noopener">Zie hier het volledige WOS artikel.</a>
+          <a
+            href="https://www.wos.nl/nk-kanopolo-op-de-wen-heel-fysiek-je-mag-heel-veel/nieuws/item?1142346"
+            target="_blank"
+            rel="noopener"
+            >Zie hier het volledige WOS artikel.</a
+          >
         </p>
       </article>
 
-      <xsystems-google-calendar narrow
-                                ?hidden=${this.narrow}
-                                .calendars=${this._calendars}
-                                time-zone="Europe/Amsterdam"
-                                language="nl">
+      <xsystems-google-calendar
+        narrow
+        ?hidden=${this.narrow}
+        .calendars=${this._calendars}
+        time-zone="Europe/Amsterdam"
+        language="nl"
+      >
       </xsystems-google-calendar>
     `;
   }
