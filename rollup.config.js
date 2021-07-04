349d195d212ae906ea627ee669ddc282082a1c3b
Koen Boes <kboes@qualogy.com>
2021-07-04 02:14:40 +0200

    Generate Service Worker during build

--- rollup.config.js
+++ rollup.config.js
@@ -4,11 +4,14 @@ import html from '@web/rollup-plugin-html';
 import minifyHTML from 'rollup-plugin-minify-html-literals';
 import summary from 'rollup-plugin-summary';
 import { terser } from 'rollup-plugin-terser';
+import { generateSW } from 'rollup-plugin-workbox';
+
+const BUILD_DIRECTORY = 'build/es-bundled';
 
 export default {
   input: 'index.html',
   output: {
-    dir: 'build/es-bundled',
+    dir: `${BUILD_DIRECTORY}`,
     format: 'es',
   },
   plugins: [
@@ -36,6 +39,10 @@ export default {
         'package.json',
         'robots.txt',
       ],
+    }),
+    generateSW({
+      swDest: `${BUILD_DIRECTORY}/service-worker.js`,
+      globDirectory: `${BUILD_DIRECTORY}`,
     }),
   ],
   preserveEntrySignatures: 'strict',
